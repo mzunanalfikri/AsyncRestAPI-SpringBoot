f00d4199d7f540880af4f5fb8b3e5c099c7157b9
mzunanalfikri <zunanalfikri@gmail.com>
2020-06-15 19:42:28 +0700

    Spring Boot template

---
+++ src/main/java/com/gdplabs/AsyncRestAPI/AsyncRestApiApplication.java
@@ -0,0 +1,14 @@
+package com.gdplabs.AsyncRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncRestApiApplication.class, args);
+		System.out.println("yey success");
+	}
+
+}
---
+++ src/test/java/com/gdplabs/AsyncRestAPI/AsyncRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdplabs.AsyncRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
