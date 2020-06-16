b9056f221b7da8363b74d062c992c849fb417610
mzunanalfikri <zunanalfikri@gmail.com>
2020-06-16 08:41:18 +0700

    add post completablefuture

--- src/main/java/com/gdplabs/AsyncRestAPI/controller/UserController.java
+++ src/main/java/com/gdplabs/AsyncRestAPI/controller/UserController.java
@@ -1,20 +1,42 @@
 package com.gdplabs.AsyncRestAPI.controller;
 
 import com.gdplabs.AsyncRestAPI.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class UserController {
+    public static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService service;
 
     @GetMapping(value = "/users")
     public CompletableFuture<ResponseEntity> findAllUsers(){
         return service.findAllUser().thenApply(ResponseEntity::ok); //async method
+    }
+
+    @PostMapping(value="/users", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = "application/json")
+    public ResponseEntity saveUsersCSV(@RequestParam(value = "files")MultipartFile[] files) throws Exception {
+        logger.info("post a file ...");
+        int i = 1;
+        for (MultipartFile file : files){
+            logger.info("start parsing file {}", i);
+            service.saveUserFromCSV(file); // async completablefuture method
+            logger.info("done saving");
+            i++;
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- src/main/java/com/gdplabs/AsyncRestAPI/service/UserService.java
+++ src/main/java/com/gdplabs/AsyncRestAPI/service/UserService.java
@@ -2,12 +2,18 @@ package com.gdplabs.AsyncRestAPI.service;
 
 import com.gdplabs.AsyncRestAPI.models.User;
 import com.gdplabs.AsyncRestAPI.repositories.UserRepository;
+import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -23,10 +29,48 @@ public class UserService {
         logger.info(" get all user by thread : " + Thread.currentThread().getName());
         List<User> users = repository.findAll();
         try {
-            Thread.sleep(5000);
+            Thread.sleep(5000); // sleep for calculations
         } catch (InterruptedException e){
 
         }
         return CompletableFuture.completedFuture(users);
+    }
+
+    //save users to database from file csv
+    @Async
+    public CompletableFuture<List<User>> saveUserFromCSV(MultipartFile file) throws Exception{
+        long start = System.currentTimeMillis();
+        List<User> users = parseCSVFile(file);
+        logger.info("saving list of users of size {}" , users.size(), ". Thread work : " + Thread.currentThread().getName());
+        repository.saveAll(users);
+        long end = System.currentTimeMillis();
+        logger.info("Total time to save : {}", (start-end));
+        return CompletableFuture.completedFuture(users);
+    }
+
+    private List<User> parseCSVFile(final MultipartFile file) throws Exception {
+        final List<User> users = new ArrayList<>();
+        logger.info("Start parsing CSV file");
+        try {
+
+            try (final BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()))){
+                String line;
+                while ((line = br.readLine()) != null ){
+                    final String[] data = line.split(",");
+                    final User user = new User();
+                    user.setId(ObjectId.get().toString());
+                    user.setName(data[0]);
+                    user.setEmail(data[1]);
+                    user.setGender(data[2]);
+                    user.setCompany(data[3]);
+                    users.add(user);
+                }
+                System.out.println("parse csv done");
+                return users;
+            }
+        } catch (final IOException e){
+            logger.error("Failed to parse csv file {}", e);
+            throw new Exception("Failed to parse CSV file {}", e);
+        }
     }
 }
